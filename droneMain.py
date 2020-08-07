c9d6618dc16ca109b6e85f3d7bbd1142fb874e00
Kostantinos Karapas <DatSpace@users.noreply.github.com>
2020-08-07 03:21:19 +0100

    Weekly big update

--- droneMain.py
+++ droneMain.py
@@ -18,6 +18,8 @@ import numpy as np
 SCR_WIDTH = 512
 SCR_HEIGHT = 512
 SIMULATION_STEP = 50.0  # in milliseconds
+DRONE_GOAL_HEIGHT = 8.0
+ROBOT_RADIUS_PIXELS = 10  # Based on an image of 512x512
 
 
 def heurestic(start, goal):
@@ -65,28 +67,83 @@ def find_shortest_path(maze_map, start, goal):
             visited_nodes.append(best_node)
     return path
 
+# Processes the binary image to account for the size of the ground robot
+
+
+def proccessToFinalMap(binary_map):
+    print("Started processing of binary image...")
+    final_map = np.copy(binary_map)
+
+    # 104,857,600 iterations :D :D :D
+    for i in range(SCR_WIDTH):
+        for j in range(SCR_HEIGHT):
+            if (binary_map[i][j] == 255):
+                for k in range(-ROBOT_RADIUS_PIXELS, ROBOT_RADIUS_PIXELS + 1):
+                    x = i + k
+                    for l in range(-ROBOT_RADIUS_PIXELS, ROBOT_RADIUS_PIXELS + 1):
+                        y = j + l
+                        if (x >= 0 and y >= 0 and x < SCR_WIDTH and y < SCR_HEIGHT):
+                            if ((k ** 2.0) + (l ** 2.0) <= ROBOT_RADIUS_PIXELS ** 2.0):
+                                if (final_map[x][y] == 0):
+                                    final_map[x][y] = 255
+    cv2.imshow("Processed Map", final_map)
+    return final_map
+
 # Converts the pixel coordinates from the map to world coordinates for the robots
 
 
-def cameraToWorldCord2D(current_pos, camera_min, camera_max, world_min, world_max):
+def cameraToWorldCoord2D(current_pos, camera_min, camera_max, world_min, world_max):
     mapX = (current_pos[0] - camera_min[0]) * (world_max[0] -
                                                world_min[0]) / (camera_max[0] - camera_min[0]) + world_min[0]
     mapY = (current_pos[1] - camera_min[1]) * (world_max[1] -
                                                world_min[1]) / (camera_max[1] - camera_min[1]) + world_min[1]
-    return np.array([mapX, mapY, current_pos[2]])
+    return np.array([mapX, mapY])
 
 
-def getFilteredMap(image):
+def worldToCameraCoord2D(current_pos, world_min, world_max, camera_min, camera_max):
+    mapX = int((current_pos[0] - world_min[0]) * (world_max[0] -
+                                                  camera_min[0]) / (camera_max[0] - world_min[0]) + camera_min[0])
+    mapY = int((current_pos[1] - world_min[1]) * (world_max[1] -
+                                                  camera_min[1]) / (camera_max[1] - world_min[1]) + camera_min[1])
+    return np.array([mapX, mapY])
+
+
+def calculateMoments(image):
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
     red_mask1 = cv2.inRange(hsv, np.array(
-        [0, 100, 50]), np.array([15, 255, 255]))
+        [0, 100, 50]), np.array([10, 255, 255]))
     red_mask2 = cv2.inRange(hsv, np.array(
         [170, 100, 50]), np.array([180, 255, 255]))
     red_mask = red_mask1 | red_mask2
 
-    blue_mask = cv2.inRange(hsv, np.array(
-        [100, 100, 50]), np.array([135, 255, 255]))
+    red_filtered = cv2.bitwise_and(image, image, mask=red_mask)
+
+    gray_image = cv2.cvtColor(red_filtered, cv2.COLOR_BGR2GRAY)
+
+    # convert the grayscale image to binary image
+    ret, thresh = cv2.threshold(gray_image, 0, 255, 0)
+
+    # calculate moments of binary image
+    return cv2.moments(thresh)
+
+
+def getCarPixelCentre(original):
+    M = calculateMoments(original)
+
+    # calculate x,y coordinate of center
+    if (M["m00"] != 0):
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+    else:
+        cX = -1
+        cY = -1
+    # car_world_location = cameraToWorldCoord2D([cX, cY], [0.0, 0.0], [SCR_WIDTH, SCR_HEIGHT], [-10.0, -10.0], [10.0, 10.0])
+    return [cX, cY]
+
+
+def getFilteredMap(image):
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
     green_mask = cv2.inRange(hsv, np.array(
         [45, 0, 0]), np.array([65, 255, 255]))
@@ -94,7 +151,7 @@ def getFilteredMap(image):
     white_mask = cv2.inRange(hsv, np.array(
         [0, 0, 0]), np.array([0, 0, 255]))
 
-    mask = red_mask | blue_mask | green_mask | white_mask
+    mask = green_mask | white_mask
 
     filtered_image = cv2.bitwise_and(image, image, mask=mask)
 
@@ -108,7 +165,7 @@ def getBinaryMap(image):
 
 
 def moveToCentre(clientID, drone_target, drone_target_position):
-    if (round(drone_target_position[2], 3) != 9.0):
+    if (round(drone_target_position[2], 3) != DRONE_GOAL_HEIGHT):
         drone_target_position = flyUp(
             clientID, drone_target, drone_target_position, 0.01)
     if (round(drone_target_position[1], 3) != 0.0):
@@ -171,8 +228,10 @@ def flyBackward(clientID, drone_target, drone_target_position, step):
 
 
 def main(drone_queue):
-
     drone_target_position = np.array([0, 0, 0])
+    binary_map = np.array([])
+    start_point = [None, None]
+    end_point = [None, None]
 
     # Start Program and just in case, close all opened connections
     print('Program started...')
@@ -212,57 +271,48 @@ def main(drone_queue):
 
         drone_target_res = sim.simx_return_novalue_flag
 
-        # Testing print statment
-        print(cameraToWorldCord2D([128.0, 346.0, 8.0], [0.0, 0.0], [
-              512.0, 512.0], [-10.0, -10.0], [10.0, 10.0]))
-
         # While connected to the simulator
         while (sim.simxGetConnectionId(clientID) != -1):
             start_ms = int(round(time.time() * 1000))
 
-            # Elevate drone and start scanning movement
-            if (drone_target_res is sim.simx_return_ok):
-
-                if (not np.allclose(drone_target_position, [0.0, 0.0, 9.0])):
-                    drone_target_position = moveToCentre(
-                        clientID, drone_target, drone_target_position)
-                else:
-                    # Get the image from the camera
-                    res, resolution, image = sim.simxGetVisionSensorImage(
-                        clientID, drone_camera, 0, sim.simx_opmode_buffer)
-                    if res == sim.simx_return_ok:
-                        # Convert from V-REP representation to OpenCV
-                        original_image = np.array(image, dtype=np.uint8)
-                        original_image.resize(
-                            [resolution[0], resolution[1], 3])
-
-                        original_image = cv2.flip(original_image, 0)
-                        original_image = cv2.cvtColor(
-                            original_image, cv2.COLOR_RGB2BGR)
-
-                        filtered_map = getFilteredMap(original_image)
-                        binary_map = getBinaryMap(filtered_map)
-
-                        cv2.imshow("Drone View", original_image)
-                        cv2.imshow("Filtered View", filtered_map)
-                        cv2.imshow("Map View", binary_map)
-
-                    elif res == sim.simx_return_novalue_flag:
-                        # Camera has not started or is not returning images
-                        print("No image yet...")
-                    else:
-                        # Something else has happened
-                        print("Unexpected error returned...", res)
-
-                # if (round(drone_target_position[2], 3) == 5.0):
-                #     drone_target_position=scanMap(
-                #         clientID, drone_target, drone_target_position)
-                # else:
-                #     drone_target_position=flyUp(
-                #         clientID, drone_target, drone_target_position, 0.01)
+            if (binary_map.size != 0):
+                cv2.imshow("Map View", binary_map)
+                pass
             else:
-                drone_target_res, drone_target_position = sim.simxGetObjectPosition(
-                    clientID, drone_target, -1, sim.simx_opmode_oneshot)
+                # Elevate drone and start scanning movement
+                if (drone_target_res is sim.simx_return_ok):
+                    if (not np.allclose(drone_target_position, [0.0, 0.0, DRONE_GOAL_HEIGHT])):
+                        drone_target_position = moveToCentre(
+                            clientID, drone_target, drone_target_position)
+                    else:
+                        # Wait for 10 seconds to allow drone to stabilize
+                        time.sleep(10)
+                        # Get the image from the camera
+                        res, resolution, image = sim.simxGetVisionSensorImage(
+                            clientID, drone_camera, 0, sim.simx_opmode_buffer)
+                        if res == sim.simx_return_ok:
+                            print("Captured snapshot of map...")
+
+                            # Convert from V-REP representation to OpenCV
+                            original_image = np.array(image, dtype=np.uint8)
+                            original_image.resize(
+                                [resolution[0], resolution[1], 3])
+
+                            original_image = cv2.flip(original_image, 0)
+                            original_image = cv2.cvtColor(
+                                original_image, cv2.COLOR_RGB2BGR)
+
+                            end_point = getCarPixelCentre(original_image)
+
+                            filtered_map = getFilteredMap(original_image)
+                            binary_map = getBinaryMap(filtered_map)
+
+                            proccessToFinalMap(binary_map)
+                else:
+                    drone_target_res, drone_target_position = sim.simxGetObjectPosition(
+                        clientID, drone_target, -1, sim.simx_opmode_oneshot)
+                    start_point = worldToCameraCoord2D([drone_target_position[0], drone_target_position[1]], [
+                                                       -10.0, -10.0], [10.0, 10.0], [0.0, 0.0], [SCR_WIDTH, SCR_HEIGHT])
 
             key = cv2.waitKey(1) & 0xFF
             if (key == ord('q')):
