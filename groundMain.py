04be34eb9472412702a6e8d511829395d6fb50cd
Mikekd <micc.822@gmail.com>
2020-08-11 12:52:36 -0500

    2

--- groundMain.py
+++ groundMain.py
@@ -4,7 +4,7 @@
 # Make sure to have the server side running in CoppeliaSim:
 # in a child script of a CoppeliaSim scene, add following command
 # to be executed just once, at simulation start:
-# Miguel
+# Miguel Cadenas
 # simRemoteApi.start(19999)
 #
 # then start simulation, and run this program.
